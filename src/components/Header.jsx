ec04ed34b6a2e2415f4e6de3d71b8b6f07ea0b0b
Fran Rubio <frubio1207@gmail.com>
2024-11-01 11:41:23 +0100

    Modificacion Bootstrap col-6 menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,12 +9,12 @@ export default function Header({cart, removeFromCart, increaseQuantity, decrease
 <header className="py-5 header">
     <div className="container-xl">
         <div className="row justify-content-center justify-content-md-between">
-            <div className="col-8 col-md-3">
+            <div className="col-6">
                 <a href="index.html">
                     <img className="img-fluid" src="/img/logo-surf.png" alt="imagen logo" />
                 </a>
             </div>
-            <nav className="col-md-6 a mt-5 d-flex align-items-start justify-content-end">
+            <nav className="col-6 a mt-5 d-flex align-items-start justify-content-end">
                 <div className="carrito">
                     <img className="img-fluid" src="/img/carrito.png" alt="imagen carrito" />
                     <div id="carrito" className="bg-white p-3">
